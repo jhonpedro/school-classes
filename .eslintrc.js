16b100a61b27b861024c73294f597de3b15fcf68
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-24 18:17:36 -0300

    removing prettier and fixing bugs in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,20 @@
-export default {
-	env: {
-		es2020: true,
-		node: true,
-	},
-	extends: [
-		'airbnb-base',
-		'prettier',
-	],
-	parserOptions: {
-		ecmaVersion: 12,
-		sourceType: 'module',
-		parser: 'babel-eslint',
-	},
-	rules: {
-		indent: [
-			2,
-			'tab',
-		],
-		'no-tabs': 0,
-		semi: [
-			'error',
-			'never',
-		],
-		'no-console': 0,
-		'prettier/prettier': 'error',
-		'import/prefer-default-export': 'off',
-	},
-}
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    semi: 0,
+    'no-console': 0,
+    'class-methods-use-this': 0,
+    'arrow-parens': 0,
+    'import/first': 0,
+  },
+};
