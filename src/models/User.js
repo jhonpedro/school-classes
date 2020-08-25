2dee3ff6c55bb566010c6554c7a7275507fb7828
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-25 19:42:42 -0300

    adding validations for users fields with sequelize

--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,44 @@ import Sequelize, { Model } from 'sequelize'
 export default class User extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password_hash: Sequelize.STRING,
-      password: Sequelize.VIRTUAL,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: true,
+          len: {
+            args: [4, 255],
+            msg: 'Name must at least 4 and 255 of maximum caracheters',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'Invalid email',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: true,
+        },
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          notEmpty: true,
+          len: {
+            args: [6, 30],
+            msg: 'Password must have at least 6 and 30 maximum carachters',
+          },
+        },
+      },
     }, {
       sequelize,
     })
