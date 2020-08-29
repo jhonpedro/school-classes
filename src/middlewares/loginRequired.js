e3e6647133f71ae89199cfb4bb1eb0f0976b70a0
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-29 18:25:54 -0300

    creating a loginRequired middleware

---
+++ src/middlewares/loginRequired.js
@@ -0,0 +1,28 @@
+import JWT from 'jsonwebtoken'
+
+export default function MiddlewareLoginRequired(req, res, next) {
+  const { authorization } = req.headers
+
+  if (!authorization) {
+    return res.status(401).json({ error: 'unauthorized' })
+  }
+
+  const [, token] = authorization.split(' ')
+
+  try {
+    const tokenData = JWT.verify(token, process.env.JWT_SECRET)
+
+    const { id, name, email } = tokenData
+    req.user = {
+      id,
+      name,
+      email,
+    }
+
+    console.log(id, name, email)
+    next()
+  } catch (error) {
+    console.log(error)
+    return res.status(401).json({ error: error.message })
+  }
+}
