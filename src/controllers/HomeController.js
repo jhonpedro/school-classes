62ac00c3b28d2a0ad6d5d22eb86a019058aec3f6
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-29 18:25:20 -0300

    removing creation of a student in home controller

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,15 +1,6 @@
-import Student from '../models/Student'
-
 class HomeController {
   async index(req, res) {
-    const newStudent = await Student.create({
-      name: 'Joao',
-      age: 12,
-      email: 'test',
-      last_name: 'pedro',
-    })
-
-    res.json({ newStudent })
+    res.json('Home')
   }
 }
 
