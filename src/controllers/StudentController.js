86796ee98ec4a4d868da4fec0e6aa1c7f1b62862
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-30 20:50:27 -0300

    creating a CRUD in student controller

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -5,6 +5,92 @@ class StudentsController {
     const student = await Student.findAll()
     res.json(student)
   }
+
+  async store(req, res) {
+    const {
+      name, lastName, email, age,
+    } = req.body
+
+    if (!name || !lastName || !email || !age) {
+      return res.status(400).json({ error: 'missing params' })
+    }
+
+    try {
+      const newStudent = await Student.create({
+        name,
+        last_name: lastName,
+        email,
+        age,
+      })
+
+      return res.json(newStudent)
+    } catch (error) {
+      console.log(error)
+      return res.status(404).json({ error: error.message })
+    }
+  }
+
+  async show(req, res) {
+    const { id } = req.params
+    try {
+      if (!id) {
+        res.status(400).json({ error: 'missing params' })
+      }
+
+      const student = await Student.findByPk(id)
+
+      return res.json(student)
+    } catch (error) {
+      console.log(error)
+      return res.status(404).json({ error: error.message })
+    }
+  }
+
+  async update(req, res) {
+    const { id } = req.params
+
+    const {
+      name, lastName, email, age,
+    } = req.body
+
+    try {
+      if (!id) {
+        res.status(400).json({ error: 'missing params' })
+      }
+
+      const student = await Student.update({
+        name,
+        lastName,
+        email,
+        age,
+      }, {
+        where: {
+          id,
+        },
+      })
+
+      return res.json(student)
+    } catch (error) {
+      console.log(error)
+      return res.status(404).json({ error: error.message })
+    }
+  }
+
+  async delete(req, res) {
+    const { id } = req.params
+
+    if (!id) {
+      res.status(400).json({ error: 'missing params' })
+    }
+
+    const operationIsConcluded = await Student.destroy({
+      where: {
+        id,
+      },
+    })
+
+    return res.json(operationIsConcluded === 1)
+  }
 }
 
 export default new StudentsController()
