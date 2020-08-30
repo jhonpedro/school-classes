9ce6ef289cffb2abb883fd063bdf32689bdecfaa
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-30 20:48:50 -0300

    adding validations for student model

--- src/models/Student.js
+++ src/models/Student.js
@@ -3,10 +3,44 @@ import Sequelize, { Model } from 'sequelize'
 export default class Student extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      last_name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      age: Sequelize.INTEGER,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'the name must have at least 3 and 255 of maximum carachters',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'invalid email',
+          },
+        },
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'the last name must have at least 3 and 255 of maximum carachters',
+          },
+        },
+      },
+      age: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'age must be a integer number',
+          },
+        },
+      },
     }, {
       sequelize,
     })
