8d41a9e612637ad7ff436bb70621ff9699907922
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-30 20:52:35 -0300

    creating seeds for student and user

---
+++ src/database/seed/20200830222958-create-users.js
@@ -0,0 +1,29 @@
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert('users',
+    [
+      {
+        name: 'Joao',
+        password_hashed: '$2a$10$Nq27CgtWTf5oFiepxzIV9etcSDJiQCDs.hVzghRHdcUsAEjgp02Uu',
+        email: 'joao@joao.com',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Maria',
+        password_hashed: '$2a$10$Nq27CgtWTf5oFiepxzIV9etcSDJiQCDs.hVzghRHdcUsAEjgp02Uu',
+        email: 'maria@maria.com',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Arthur',
+        password_hashed: '$2a$10$Nq27CgtWTf5oFiepxzIV9etcSDJiQCDs.hVzghRHdcUsAEjgp02Uu',
+        email: 'arthur@arthur.com',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {}),
+
+  down: async (queryInterface) => queryInterface.bulkDelete('users', null, {}),
+};
---
+++ src/database/seed/20200830224448-create-students.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert('students',
+    [
+      {
+        name: 'Joao',
+        last_name: 'Pedro',
+        age: 17,
+        email: 'joao@joao.com',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Maria',
+        last_name: 'José',
+        age: 25,
+        email: 'maria@maria.com',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Arthur',
+        last_name: 'Marinho',
+        age: 29,
+        email: 'arthur@arthur.com',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {}),
+
+  down: async (queryInterface) => queryInterface.bulkDelete('students', null, {}),
+};
