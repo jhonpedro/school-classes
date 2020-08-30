0ac88cffa9de30a4531b772ce5f95ba0394d87c7
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-30 20:51:56 -0300

    creating migration for student email be unique

---
+++ src/database/migrations/20200829214927-change-student-email-unique.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.changeColumn(
+    'students',
+    'email',
+    {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  ),
+
+  down: async (queryInterface, Sequelize) => queryInterface.changeColumn(
+    'students',
+    'email',
+    {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  ),
+};
