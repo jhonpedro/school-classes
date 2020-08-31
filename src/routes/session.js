4e605af5c4b9915aaa738b59e30cb33fe650c562
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-30 21:37:33 -0300

    changing filenames

--- src/routes/session.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import sessionController from '../controllers/SessionController'
-
-const router = new Router()
-
-router.post('/', sessionController.login)
-
-export default router;
