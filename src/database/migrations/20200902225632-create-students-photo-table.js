0ca33a347008ea6c8c34a296515e64343a31899c
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-02 20:06:17 -0300

    migration students_photo table

---
+++ src/database/migrations/20200902225632-create-students-photo-table.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.createTable('students_photo', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    originalName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    filename: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    student_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'students',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: async (queryInterface) => queryInterface.dropTable('students_photo'),
+};
