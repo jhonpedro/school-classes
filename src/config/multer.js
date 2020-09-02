15136eb4bbb998ecd5df317bfd6fd75137162cf7
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-02 19:50:41 -0300

    filtering to only image files to not throw

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,6 +4,13 @@ import { extname, resolve } from 'path'
 import randomNumber from '../utils/randomNumber'
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png ' && file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/jpg') {
+      return cb(new multer.MulterError('file must be an image'))
+    }
+
+    return cb(null, true)
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'))
