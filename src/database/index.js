887e009d74ad60b0596a13c23650276b63bc4f8a
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-03 19:02:13 -0300

    adding photo model

--- src/database/index.js
+++ src/database/index.js
@@ -3,9 +3,11 @@ import databaseConfig from '../config/database'
 
 import Student from '../models/Student'
 import User from '../models/User'
+import Photo from '../models/Photo'
 
-const models = [Student, User]
+const models = [Student, User, Photo]
 
 const connection = new Sequelize(databaseConfig)
 
 models.forEach(model => model.init(connection))
+models.forEach(model => model.associate && model.associate(connection.models))
