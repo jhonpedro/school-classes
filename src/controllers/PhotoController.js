b8305eadff0bf59db2ff14b3207696b3d9aec547
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-03 19:01:57 -0300

    store method returns the created photo

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,17 +1,31 @@
 import multer from 'multer'
 import multerConfig from '../config/multer'
+import Photo from '../models/Photo'
 
 const upload = multer(multerConfig).single('photo')
 
 class HomeController {
-  async store(req, res) {
-    return upload(req, res, (error) => {
+  store(req, res) {
+    return upload(req, res, async (error) => {
       if (error) {
         console.log(error)
         return res.status(400).json({ error: error.message })
       }
+      try {
+        const { originalname, filename } = req.file
+        const { id } = req.body
 
-      return res.json('ok')
+        const photo = await Photo.create({
+          originalname,
+          filename,
+          student_id: id,
+        })
+
+        return res.json(photo)
+      } catch (errorFromCatch) {
+        console.log(errorFromCatch)
+        return res.status(400).json({ error: 'something went wrong' })
+      }
     })
   }
 }
