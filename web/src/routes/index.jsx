4506106670d97f36e4236ee1e7d975630452bddc
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-08 19:03:19 -0300

    creating routes file

---
+++ web/src/routes/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
+
+import history from '../services/history'
+import Login from '../pages/Login'
+import Landing from '../pages/Landing'
+import Page404 from '../pages/Page404'
+import Header from '../components/Header'
+
+function Routes() {
+	return (
+		<Router history={history}>
+			<Switch>
+				<Route path="/" exact>
+					<Landing />
+				</Route>
+
+				<Route path="/login">
+					<Header showLogin />
+					<Login />
+				</Route>
+
+				<Route path="*">
+					<Header showLogin />
+					<Page404 />
+				</Route>
+			</Switch>
+		</Router>
+	)
+}
+
+export default Routes
