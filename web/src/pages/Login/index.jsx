7fc4bc24d831e9a0f9b48c89c5fc2e3594d33072
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-08 19:02:41 -0300

    creating template for login page

---
+++ web/src/pages/Login/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Title, Container } from './styles'
+
+function LoginPage() {
+	return (
+		<>
+			<Container>
+				<Title>Login 1</Title>
+			</Container>
+		</>
+	)
+}
+
+export default LoginPage
---
+++ web/src/pages/Login/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0 auto;
+	margin-top: 80px;
+	width: 40%;
+	height: 40%;
+	box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
+	background-color: #a1a1a1;
+	border-radius: 0.5rem;
+`
+
+export const Title = styled.h1`
+	color: #000a15;
+`
