fc99016816a7ffd50b070cbca36981bc95a06f79
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-08 19:01:35 -0300

    creating 404 page

---
+++ web/src/pages/Page404/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Container, Content } from './styles'
+
+function Page404() {
+	return (
+		<Container>
+			<Content>
+				<p className="colorful">
+					Opá, parece que você chegou na borda da nossa plataforma
+				</p>
+				Tente voltar pra página principal, por aqui não há nada
+			</Content>
+		</Container>
+	)
+}
+
+export default Page404
---
+++ web/src/pages/Page404/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+	width: 100vw;
+	height: 85vh;
+	padding: 0 2rem;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	color: var(--color-text-white);
+`
+
+export const Content = styled.article`
+	.colorful {
+		color: var(--color-primary);
+		font-size: 3rem;
+	}
+	font-size: 2rem;
+	font-family: Poppins;
+`
