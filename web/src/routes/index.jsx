c7d0a784a041127a9127fb43af49ebb81680ccee
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-11 15:50:10 -0300

    adding customized route to routes index

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Router, Route, Switch } from 'react-router-dom'
+import { Router, Switch } from 'react-router-dom'
 
+import Route from './Route'
 import history from '../services/history'
 import Login from '../pages/Login'
 import Landing from '../pages/Landing'
