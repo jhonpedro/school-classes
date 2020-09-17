617c8e60e1b53447e634053abc5a6caf4e4b403b
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-16 23:05:12 -0300

    removing tests

--- web/src/pages/Page404/index.jsx
+++ web/src/pages/Page404/index.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-
 import { Container, Content } from './styles'
-import * as actionTypes from '../../store/modules/sample/actions'
 
 function Page404() {
-	const dispatch = useDispatch((state) => state.sampleReducer.click)
-
-	function handleClick() {
-		dispatch(actionTypes.buttonClickRequest())
-	}
 	return (
 		<Container>
 			<Content>
@@ -17,9 +9,6 @@ function Page404() {
 					Opá, parece que você chegou na borda da nossa plataforma
 				</p>
 				Tente voltar pra página principal, por aqui não há nada
-				<button type="button" onClick={handleClick}>
-					Clique
-				</button>
 			</Content>
 		</Container>
 	)
