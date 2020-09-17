1f058aa01f6d382410c8872a3da44d529893a79d
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-16 23:04:49 -0300

    adding inputs and styles

--- web/src/pages/Login/index.jsx
+++ web/src/pages/Login/index.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 
-import { Title, Container } from './styles'
+import Header from '../../components/Header'
+import Input from '../../components/Input'
+
+import {
+	LoginContainer,
+	LoginContainerContent,
+	LoginBox,
+	SideText,
+} from './styles'
 
 function LoginPage() {
 	return (
 		<>
-			<Container>
-				<Title>Login 1</Title>
-			</Container>
+			<LoginContainer>
+				<Header showLogin />
+				<LoginContainerContent className="container">
+					<SideText>
+						Login <br />
+						<p className="downText">Digite seus dados para efetuar o login</p>
+					</SideText>
+					<LoginBox>
+						<Input placeholder="E-mail" />
+						<Input placeholder="Senha" />
+					</LoginBox>
+				</LoginContainerContent>
+			</LoginContainer>
 		</>
 	)
 }
--- web/src/pages/Login/styles.js
+++ web/src/pages/Login/styles.js
@@ -1,18 +1,49 @@
 import styled from 'styled-components'
 
-export const Container = styled.main`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin: 0 auto;
-	margin-top: 80px;
-	width: 40%;
-	height: 40%;
-	box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
-	background-color: #a1a1a1;
-	border-radius: 0.5rem;
+export const LoginContainer = styled.main`
+	width: 100vw;
+	height: 100vh;
 `
 
-export const Title = styled.h1`
-	color: #000a15;
+export const LoginContainerContent = styled.main`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	margin: 0 auto;
+
+	height: 90vh;
+
+	@media (min-width: 992px) {
+		flex-direction: row;
+		justify-content: space-between;
+	}
+`
+export const SideText = styled.div`
+	font-size: 4.5rem;
+	color: var(--color-primary);
+	max-width: 50rem;
+
+	.downText {
+		font-size: 2.5rem;
+		color: var(--color-text-white);
+	}
+`
+
+export const LoginBox = styled.form`
+	height: 35%;
+	width: 100%;
+	margin-top: 2rem;
+
+	display: flex;
+	align-items: center;
+	justify-content: space-evenly;
+	flex-direction: column;
+	padding: 4rem;
+	background-color: var(--color-primary);
+	border-radius: 0.9rem;
+
+	@media (min-width: 992px) {
+		width: 50%;
+	}
 `
