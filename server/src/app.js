c01a85b6330dc07fcc4eaf54b526750f19c096d3
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-23 17:03:39 -0300

    adding cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,17 @@
 import dotenv from 'dotenv'
 import { resolve } from 'path'
+import cors from 'cors'
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		const whiteList = ['http://localhost:3000']
+		if (whiteList.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by cors'))
+		}
+	},
+}
 
 dotenv.config()
 
@@ -13,25 +25,26 @@ import SessionRoute from './routes/SessionRoutes'
 import PhotoRoute from './routes/PhotoRoutes'
 
 class App {
-  constructor() {
-    this.app = express();
-    this.middlewares()
-    this.routes()
-  }
+	constructor() {
+		this.app = express()
+		this.middlewares()
+		this.routes()
+	}
 
-  middlewares() {
-    this.app.use(express.urlencoded({ extended: true }))
-    this.app.use(express.json())
-    this.app.use(express.static(resolve(__dirname, '..', 'uploads')))
-  }
+	middlewares() {
+		this.app.use(cors())
+		this.app.use(express.urlencoded({ extended: true }))
+		this.app.use(express.json())
+		this.app.use(express.static(resolve(__dirname, '..', 'uploads')))
+	}
 
-  routes() {
-    this.app.use(HomeRoute)
-    this.app.use('/users', UserRoute)
-    this.app.use('/session', SessionRoute)
-    this.app.use('/students', StudentRoutes)
-    this.app.use('/photos', PhotoRoute)
-  }
+	routes() {
+		this.app.use(HomeRoute)
+		this.app.use('/users', UserRoute)
+		this.app.use('/session', SessionRoute)
+		this.app.use('/students', StudentRoutes)
+		this.app.use('/photos', PhotoRoute)
+	}
 }
 
 export default new App().app
