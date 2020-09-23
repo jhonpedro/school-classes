ac36e508d7cbc4bfd0c49e52840947e10d5fd716
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-23 17:01:11 -0300

    adding user on response

--- server/src/controllers/SessionController.js
+++ server/src/controllers/SessionController.js
@@ -2,47 +2,50 @@ import JWT from 'jsonwebtoken'
 import User from '../models/User'
 
 class SessionController {
-  async login(req, res) {
-    const { email: reqEmail, password } = req.body
+	async login(req, res) {
+		const { email: reqEmail, password } = req.body
 
-    try {
-      if (!reqEmail || !password) {
-        return res.status(401).json({ error: 'missing params' })
-      }
+		try {
+			if (!reqEmail || !password) {
+				return res.status(401).json({ error: 'missing params' })
+			}
 
-      const user = await User.findOne({
-        where: {
-          email: reqEmail,
-        },
-      })
+			const user = await User.findOne({
+				where: {
+					email: reqEmail,
+				},
+			})
 
-      const isPasswordValid = user && (await user.passwordIsValid(password))
-      if (!user || !isPasswordValid) {
-        return res.status(400).json({ error: 'user with wrong credentials' })
-      }
+			const isPasswordValid = user && (await user.passwordIsValid(password))
+			if (!user || !isPasswordValid) {
+				return res.status(400).json({ error: 'user with wrong credentials' })
+			}
 
-      const { id, name, email } = user
+			const { id, name, email } = user
 
-      const payloadJWT = {
-        id,
-        name,
-        email,
-      }
+			const payloadJWT = {
+				id,
+				name,
+				email,
+			}
 
-      JWT.sign(payloadJWT,
-        process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_EXPIRES_IN },
-        (error, token) => {
-          if (error) return res.status(400).json({ error: 'error in token creation' })
+			JWT.sign(
+				payloadJWT,
+				process.env.JWT_SECRET,
+				{ expiresIn: process.env.JWT_EXPIRES_IN },
+				(error, token) => {
+					if (error)
+						return res.status(400).json({ error: 'error in token creation' })
 
-          return res.json({ token })
-        })
-      return null
-    } catch (error) {
-      console.log(error)
-      return res.status(400).json({ error: 'wow something went wrong' })
-    }
-  }
+					return res.json({ token, user: { id, name, email } })
+				}
+			)
+			return null
+		} catch (error) {
+			console.log(error)
+			return res.status(400).json({ error: 'wow something went wrong' })
+		}
+	}
 }
 
 export default new SessionController()
