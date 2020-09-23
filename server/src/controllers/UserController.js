d8a3b8eecc2b1e053037715a746d88ddc7f6914a
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-23 17:01:37 -0300

    refactoring

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -2,101 +2,100 @@ import bcrypt from 'bcryptjs'
 import User from '../models/User'
 
 class HomeController {
-  async store(req, res) {
-    const { name, email, password } = req.body
+	async store(req, res) {
+		const { name, email, password } = req.body
 
-    try {
-      const newUser = await User.create({
-        name,
-        email,
-        password,
-      })
+		try {
+			const newUser = await User.create({
+				name,
+				email,
+				password,
+			})
 
-      newUser.password = null
+			newUser.password = null
 
-      return res.json(newUser)
-    } catch (error) {
-      console.log(error)
-      return res.status(400).json({ error: error.message })
-    }
-  }
+			return res.json(newUser)
+		} catch (error) {
+			console.log(error)
+			return res.status(400).json({ error: error.message })
+		}
+	}
 
-  async index(req, res) {
-    try {
-      const users = await User.findAll({
-        order: [
-          ['created_at', 'ASC'],
-        ],
-      })
+	async index(req, res) {
+		try {
+			const users = await User.findAll({
+				order: [['created_at', 'ASC']],
+			})
 
-      return res.json(users)
-    } catch (error) {
-      console.log(error)
-      return res.status(400).json({ error: error.message })
-    }
-  }
+			return res.json(users)
+		} catch (error) {
+			console.log(error)
+			return res.status(400).json({ error: error.message })
+		}
+	}
 
-  async show(req, res) {
-    const { id } = req.params
+	async show(req, res) {
+		const { id } = req.params
 
-    try {
-      const user = await User.findByPk(id)
+		try {
+			const user = await User.findByPk(id)
 
-      return res.json(user)
-    } catch (error) {
-      console.log(error)
-      return res.status(400).json(null)
-    }
-  }
+			return res.json(user)
+		} catch (error) {
+			console.log(error)
+			return res.status(400).json(null)
+		}
+	}
 
-  async update(req, res) {
-    const {
-      name, lastPassword, newPassword,
-    } = req.body
-    const { id } = req.user
+	async update(req, res) {
+		const { name, lastPassword, newPassword } = req.body
+		const { id } = req.user
 
-    try {
-      const user = await User.findByPk(id)
+		try {
+			const user = await User.findByPk(id)
 
-      const isLastPasswordValid = await bcrypt.compare(lastPassword, user.password_hashed)
+			const isLastPasswordValid = await bcrypt.compare(
+				lastPassword,
+				user.password_hashed
+			)
 
-      if (!isLastPasswordValid) {
-        return res.sendStatus(401)
-      }
+			if (!isLastPasswordValid) {
+				return res.sendStatus(401)
+			}
 
-      let newUser
+			let newUser
 
-      if (name && newPassword) {
-        newUser = await user.update({ name, password: newPassword })
-      } else if (newPassword) {
-        newUser = await user.update({ password: newPassword })
-      } else {
-        newUser = await user.update({ name })
-      }
+			if (name && newPassword) {
+				newUser = await user.update({ name, password: newPassword })
+			} else if (newPassword) {
+				newUser = await user.update({ password: newPassword })
+			} else {
+				newUser = await user.update({ name })
+			}
 
-      return res.json(newUser)
-    } catch (error) {
-      console.log(error)
-      return res.status(401).json({ error: error.message })
-    }
-  }
+			return res.json(newUser)
+		} catch (error) {
+			console.log(error)
+			return res.status(401).json({ error: error.message })
+		}
+	}
 
-  async delete(req, res) {
-    const { id } = req.user
+	async delete(req, res) {
+		const { id } = req.user
 
-    try {
-      const operationIsConcluded = await User.destroy({
-        where: {
-          id,
-        },
-      })
+		try {
+			const operationIsConcluded = await User.destroy({
+				where: {
+					id,
+				},
+			})
 
-      return res.json(operationIsConcluded === 1)
-    } catch (error) {
-      console.log(error)
-      return res.status(401).json({ error: error.message })
-    }
-  }
+			return res.json(operationIsConcluded === 1)
+		} catch (error) {
+			console.log(error)
+			return res.status(401).json({ error: error.message })
+		}
+	}
 }
 
 export default new HomeController()
