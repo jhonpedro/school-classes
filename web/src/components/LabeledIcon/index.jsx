355e2625c2db31e381ad770235cbf9edec597033
Joao Pedro <pedrobarros2010@gmail.com>
2020-09-30 10:42:42 -0300

    applying rest operator

--- web/src/components/LabeledIcon/index.jsx
+++ web/src/components/LabeledIcon/index.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types'
 
 import { Container } from './styles'
 
-function LabeledIcon({ labelName, component: Icon }) {
+function LabeledIcon({ labelName, component: Icon, ...rest }) {
 	return (
-		<Container>
+		// eslint-disable-next-line react/jsx-props-no-spreading
+		<Container {...rest}>
 			{Icon}
 			<p>{labelName}</p>
 		</Container>
